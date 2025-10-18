b734fe311a178aecf9eb5e4479afa89b05861643
Christian Viegas <chris.gabriel346@gmail.com>
2025-10-18 15:25:17 -0300

    feat: adiciona a classe Shopping

---
+++ src/models/Shopping.java
@@ -0,0 +1,94 @@
+package models;
+
+import java.util.Arrays;
+
+public class Shopping {
+
+    private String nome;
+    private Endereco endereco;
+    private Loja[] lojas;
+
+    public Shopping(String nome, Endereco endereco, Integer quantidadeLojas) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.lojas = new Loja[quantidadeLojas];
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Loja[] getLojas() {
+        return lojas;
+    }
+
+    public boolean insereLoja(Loja loja) {
+        for (int i = 0; i < lojas.length; i++) {
+            if (lojas[i] == null) {
+                lojas[i] = loja;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean removeLoja(String nomeLoja) {
+        for (int i = 0; i < lojas.length; i++) {
+            if (lojas[i] != null && lojas[i].getNome().equalsIgnoreCase(nomeLoja)) {
+                lojas[i] = null;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Integer quantidadeLojasPorTipo(String tipo) {
+        int count = 0;
+        for (Loja loja : lojas) {
+            if (loja != null && loja.getClass().getSimpleName().equalsIgnoreCase(tipo)) {
+                count++;
+            }
+        }
+        if (count > 0) {
+            return count;
+        }
+        return -1;
+    }
+
+    public Informatica lojaSeguroMaisCaro() {
+        Informatica lojaMaisCara = null;
+        for (Loja loja : lojas) {
+            if (loja instanceof Informatica) {
+                if (lojaMaisCara == null) {
+                    lojaMaisCara = (Informatica) loja;
+                    continue;
+                }
+                if (((Informatica) loja).getSeguroEletronicos() > lojaMaisCara.getSeguroEletronicos()) {
+                    lojaMaisCara = (Informatica) loja;
+                }
+            }
+        }
+        return lojaMaisCara;
+    }
+
+    @Override
+    public String toString() {
+        return "Shopping{" +
+                "nome='" + nome + '\'' +
+                ", endereco=" + endereco +
+                ", lojas=" + Arrays.toString(lojas) +
+                '}';
+    }
+}
